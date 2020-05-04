9de3b42bcf212f22743467c4390ac4ba9b1c613f
Sarah Hanafy <sarahhanafy20@gmail.com>
2020-05-04 11:07:02 +0200

    Updates

---
+++ precache-manifest.8ebb7c542340d0f5e75c31b98ffa5825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc43de735f71bf023b02be879025331",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cce91e761075af381388",
+    "url": "/monsters-rolodex/static/css/main.3cd97b9d.chunk.css"
+  },
+  {
+    "revision": "198ac10f32f45d723a59",
+    "url": "/monsters-rolodex/static/js/2.17cc66d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.17cc66d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce91e761075af381388",
+    "url": "/monsters-rolodex/static/js/main.bcecd89b.chunk.js"
+  },
+  {
+    "revision": "d0027d044578a8edd0c7",
+    "url": "/monsters-rolodex/static/js/runtime-main.049a1413.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
